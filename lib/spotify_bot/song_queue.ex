f6895270ea8a457f2557d3f4429b492c1453aa31
Ryan Winchester <ryan@winchester.dev>
2024-01-24 20:04:34 -0400

    huehuehue

--- lib/spotify_bot/song_queue.ex
+++ lib/spotify_bot/song_queue.ex
@@ -2,12 +2,24 @@ defmodule SpotifyBot.SongQueue do
   @moduledoc false
   use GenServer
 
-  @default_max_total 100
-  @default_max_per_user 2
-  @default_allow_consecutive true
+  # Defaults.
+  @allow_user_consecutive false
+  @allow_consecutive false
+  @allow_duplicates false
+  @max_per_user 2
+  @max_total 100
 
   @doc """
   Start the song queue server.
+
+  ## Options
+
+   * `:max_total` - The max total queue size. Defaults to `100`.
+   * `:max_per_user` - The max number of songs per user. Defaults to `2`.
+   * `:allow_user_consecutive?` - Whether we allow consecutives per user. Defaults to `false`.
+   * `:allow_consecutive?` - Whether we allow consecutives per user. Defaults to `false`.
+   * `:allow_duplicates?` - The max total queue size. Defaults to `false`.
+
   """
   @spec start_link(keyword()) :: GenServer.on_start()
   def start_link(opts) do
@@ -18,27 +30,91 @@ defmodule SpotifyBot.SongQueue do
   Add a track to the queue.
   """
   @spec add(String.t(), String.t()) ::
-          :ok | {:error, :max_total} | {:error, :max_per_user} | {:error, :no_consecutive}
+          :ok
+          | {:error, :max_total}
+          | {:error, :max_per_user}
+          | {:error, :no_consecutive}
+          | {:error, :no_user_consecutive}
+          | {:error, :no_duplicates}
   def add(track_id, user) do
     GenServer.call(__MODULE__, {:add, track_id, user})
+  end
+
+  @doc """
+  Remove a track from the queue.
+  """
+  @spec remove(String.t()) :: :ok
+  def remove(track_id) do
+    GenServer.cast(__MODULE__, {:remove, track_id})
+  end
+
+  @doc """
+  Remove all tracks for a user.
+  """
+  @spec remove_user(String.t()) :: :ok
+  def remove_user(user) do
+    GenServer.cast(__MODULE__, {:remove_user, user})
+  end
+
+  @doc """
+  Get the size of the current queue.
+  """
+  @spec size() :: non_neg_integer()
+  def size do
+    GenServer.call(__MODULE__, :size)
   end
 
   # ----------------------------------------------------------------------------
   # Callbacks
   # ----------------------------------------------------------------------------
 
+  @doc false
   @impl GenServer
   def init(opts) do
     state = %{
-      queue: [],
-      songs_per_user: Keyword.get(opts, :max_per_user, @default_max_per_user),
-      max_total: Keyword.get(opts, :max_total, @default_max_total),
-      allow_consecutive?: Keyword.get(opts, :allow_consecutive?, @default_allow_consecutive)
+      allow_consecutive?: Keyword.get(opts, :allow_consecutive?, @allow_consecutive),
+      allow_user_consecutive?:
+        Keyword.get(opts, :allow_user_consecutive?, @allow_user_consecutive),
+      allow_duplicates?: Keyword.get(opts, :allow_duplicates?, @allow_duplicates),
+      max_total: Keyword.get(opts, :max_total, @max_total),
+      songs_per_user: Keyword.get(opts, :max_per_user, @max_per_user),
+      count: 0,
+      queue: []
     }
 
     {:ok, state}
   end
 
+  @doc false
+  @impl GenServer
+  def handle_cast({:remove, track_id}, state) do
+    state =
+      if List.keymember?(state.queue, track_id, 0) do
+        # I know this is stupid. I want to remove the last occurence and I
+        # don't want to use my brain. I'm tired. Leave me alone.
+        queue = Enum.reverse(state.queue) |> List.keydelete(track_id, 0)
+        %{state | count: state.count - 1, queue: Enum.reverse(queue)}
+      else
+        state
+      end
+
+    {:noreply, state}
+  end
+
+  def handle_cast({:remove_user, user}, state) do
+    if List.keymember?(state.queue, user, 1) do
+      {queue, deleted} =
+        Enum.reduce(state.queue, {[], 0}, fn {track, u}, {queue, deleted} ->
+          if user == u, do: {queue, deleted + 1}, else: {[{track, u} | queue], deleted}
+        end)
+
+      {:noreply, %{state | count: state.count - deleted, queue: queue}}
+    else
+      {:noreply, state}
+    end
+  end
+
+  @doc false
   @impl GenServer
   def handle_call({:add, track_id, user}, _from, state) do
     total = Enum.count(state.queue)
@@ -55,11 +131,18 @@ defmodule SpotifyBot.SongQueue do
       user_count >= state.max_per_user ->
         {:reply, {:error, :max_per_user}, state}
 
-      match?({^user, _}, hd(state.queue)) and not state.allow_consecutive? ->
+      List.keymember?(state.queue, track_id, 0) and not state.allow_duplicates? ->
+        {:reply, {:error, :no_duplicates}, state}
+
+      match?({_, ^user}, hd(state.queue)) and not state.allow_consecutive? ->
         {:reply, {:error, :no_consecutive}, state}
 
       true ->
-        {:reply, :ok, %{state | queue: [{user, track_id} | state.queue]}}
+        {:reply, :ok, %{state | count: state.count + 1, queue: [{track_id, user} | state.queue]}}
     end
+  end
+
+  def handle_call(:size, _from, state) do
+    {:reply, state.count, state}
   end
 end
